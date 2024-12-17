92d7c3c5b3f601ffe07b7f93ea773f1cde7ce7bd
Ömer Duran <me@omerduran.dev>
2024-12-17 19:33:21 +0100

    feat(ui): improvement

--- src/components/Newsletter.tsx
+++ src/components/Newsletter.tsx
@@ -15,8 +15,8 @@ export default function Newsletter({ showBackground = true }: NewsletterProps) {
     <section className="bg-[#799DBC] py-12 sm:py-20 lg:py-32" style={backgroundStyle}>
       <div className="mx-auto grid max-w-7xl grid-cols-1 gap-10 px-6 lg:grid-cols-12 lg:gap-8 lg:px-8">
         <div className="max-w-xl text-3xl font-bold tracking-tight text-white sm:text-4xl lg:col-span-7">
-          <h2 className="inline sm:block lg:inline xl:block">Want product news and updates?</h2>
-          <p className="inline sm:block lg:inline xl:block">Sign up for our newsletter.</p>
+          <h2 className="inline sm:block lg:inline xl:block">Want product news and updates? </h2>
+          <p className="inline sm:block lg:inline xl:block">Sign up for our newsletter. </p>
         </div>
         
         <form className="w-full max-w-md lg:col-span-5 lg:pt-2">
